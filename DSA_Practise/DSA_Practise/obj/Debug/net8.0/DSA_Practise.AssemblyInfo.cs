a45915786b324a49489c9479afcd9ba9d1eafe1f
Siddharth Tiwari <sidh589@gmail.com>
2024-03-02 00:57:57 +0530

    Random commit to start from new.

--- DSA_Practise/DSA_Practise/obj/Debug/net8.0/DSA_Practise.AssemblyInfo.cs
+++ DSA_Practise/DSA_Practise/obj/Debug/net8.0/DSA_Practise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA_Practise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d380992562e24f8959120ebb665ad249573208")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA_Practise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA_Practise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
