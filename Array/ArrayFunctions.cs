cfc2c6bb013a814da091c58f57cb1f8d51be5012
Siddharth Tiwari <sidh589@gmail.com>
2024-03-04 23:49:23 +0530

    Array Insertion code

---
+++ Array/ArrayFunctions.cs
@@ -0,0 +1,33 @@
+﻿namespace Array
+{
+    public class ArrayFunctions
+    {
+        public void PrintAllElemntsInAnArray(int[] array)
+        {
+            foreach (int elemnt in array)
+            {
+                Console.Write(elemnt+" ");
+            }
+            Console.WriteLine();
+        }
+
+        public void InsertElement(int[] array, int index, int elementToAdd, int filledIndex)
+        {
+            //if (filledIndex == array.Length)
+            //{
+            //    Console.Write("Couldnot fill the value as no space available \n");
+            //    return;
+            //}
+
+            for (int i = index; i <=filledIndex; i++)
+            {
+                array[i + 1] = array[i];
+            }
+
+            array[index] = elementToAdd;
+            filledIndex++;
+
+            PrintAllElemntsInAnArray(array);
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -2,6 +2,25 @@
 using Array;
 
 Console.WriteLine("Hello, World!");
-int[] array = new int[] {1,2,3,4,5 };
-Traversal traversal = new Traversal();
-traversal.PrintAllElemntsInAnArray(array);
+int[] array = new int[10];
+
+int filledElementsIndex=0;
+//Filling elements till 5th index
+for(int i=0; i<5; i++)
+{
+    array[i] = i+1;
+    filledElementsIndex=i;
+
+}
+
+ArrayFunctions functions = new ArrayFunctions();
+
+//Traverse elements of an array
+functions.PrintAllElemntsInAnArray(array);
+
+//Add element at nth index
+functions.InsertElement(array, 3, 7, filledElementsIndex);
+filledElementsIndex++;
+
+//functions.InsertElement(array, 3, 9, filledElementsIndex);
+
