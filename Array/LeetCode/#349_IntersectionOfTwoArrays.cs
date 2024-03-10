7e97b0aecdd675e981c74b6bb0c11ca6ad1ad600
Siddharth Tiwari <sidh589@gmail.com>
2024-03-10 20:56:43 +0530

    349. Intersection of Two Arrays
    HashSet method.

--- Array/LeetCode/#349_IntersectionOfTwoArrays.cs
+++ Array/LeetCode/#349_IntersectionOfTwoArrays.cs
@@ -10,25 +10,39 @@ namespace Array.LeetCode
     {
         public int[] Intersection(int[] nums1, int[] nums2)
         {
-            List<int> result = new List<int>();
-            for (int i = 0; i < nums1.Length; i++)
+            //List<int> result = new List<int>();
+            //for (int i = 0; i < nums1.Length; i++)
+            //{
+            //    for (int j = 0; j < nums2.Length; j++)
+            //    {
+            //        if (nums1[i] == nums2[j])
+            //        {
+            //            if (result.Contains(nums1[i]))
+            //                break;
+
+            //            else
+            //                result.Add(nums1[i]);
+            //        }
+
+
+            //    }
+            //}
+
+            //return result.ToArray();
+
+
+            HashSet<int> numsArry1 = new HashSet<int>(nums1);
+            HashSet<int> intersectSet = new HashSet<int>();
+
+            for (int i = 0; i < nums2.Length; i++)
             {
-                for (int j = 0; j < nums2.Length; j++)
+                if (numsArry1.Contains(nums2[i]))
                 {
-                    if (nums1[i] == nums2[j])
-                    {
-                        if (result.Contains(nums1[i]))
-                            break;
-
-                        else
-                            result.Add(nums1[i]);
-                    }
-
-
+                    intersectSet.Add(nums2[i]);
                 }
             }
 
-            return result.ToArray();
+            return intersectSet.ToArray();
 
 
         }
