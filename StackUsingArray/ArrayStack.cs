5c4fd29e5d3c883c5c39cb9acdf9193eeb9b1e7e
Siddharth Tiwari <sidh589@gmail.com>
2024-03-18 21:32:54 +0530

    Added all the operations like Push, Pop, Peek, Traverse and Clear.

--- StackUsingArray/ArrayStack.cs
+++ StackUsingArray/ArrayStack.cs
@@ -13,6 +13,23 @@
             array= new int[size];
         }
 
+        public void Traverse()
+        {
+            if (top >= 0)
+            {
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty.");
+            }
+            
+        }
+
         public bool IsEmpty()
         {
             if (top == -1)
@@ -30,19 +47,45 @@
             else return false;
         }
 
-        public void Push(int Value)
+        public void Push(int value)
         {
-
+            if (IsFull())
+            {
+                Console.WriteLine("Stack Overflow: The stack is full.");
+            }
+            else
+            {
+                array[++top] = value;
+                Console.WriteLine(value + " has been added to the stack");
+                Traverse();
+            }
+            
         }
 
         public void Pop()
         {
-
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow: No element to Pop.");
+            }
+            else
+            {
+                top--;
+                Console.WriteLine("Top value has been removed from the stack");
+                Traverse();
+            }
+            
         }
 
         public void Peek()
         {
+            if (top < 0)
+                Console.WriteLine("Stack is empty.");
 
+            else
+            {
+                Console.WriteLine("The top element is: "+array[top]);
+            }
         }
 
         public int StackTop()
@@ -53,6 +96,13 @@
         public int StackBottom()
         {
             return array[0];
+        }
+
+        public void ClearStack()
+        {
+            top = -1;
+            Console.WriteLine("The stack has been cleared.");
+            Traverse();
         }
     }
 }
--- StackUsingArray/Program.cs
+++ StackUsingArray/Program.cs
@@ -8,3 +8,14 @@ Console.WriteLine(result);
 result = stack.IsFull();
 Console.WriteLine(result);
 
+stack.Push(5);
+stack.Push(7);
+stack.Push(2);
+stack.Push(1);
+
+stack.Pop();
+
+stack.Peek();
+
+stack.ClearStack();
+
