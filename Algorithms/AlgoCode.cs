c32d9ae7e16681a919e6c0dc7001e86925a03686
Siddharth Tiwari <sidh589@gmail.com>
2024-03-27 19:22:50 +0530

    Created project for coding different Algorithm.

---
+++ Algorithms/AlgoCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class AlgoCode
+    {
+        int[] array;
+        public AlgoCode(int[] array)
+        {
+            this.array = array;
+        }
+
+        public void BubbleSort()
+        {
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+
+            }
+        }
+
+        public void InsertionSort()
+        {
+
+        }
+
+        public void Traverse()
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i+" ");
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,8 @@
+﻿using Algorithms;
+
+int[] array=new int[] {5, 31, 31,24, 14, 1};
+AlgoCode algo=new AlgoCode(array);
+
+algo.BubbleSort();
+
+algo.InsertionSort();
